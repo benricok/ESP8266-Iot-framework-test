9975a1156161352a2c8567415951ead22d24efab
Benrico Krog <62467535+benricok@users.noreply.github.com>
2021-04-25 22:38:56 +0200

    update

--- remote-esp-web/src/main.cpp
+++ remote-esp-web/src/main.cpp
@@ -1,12 +1,22 @@
 #include <Arduino.h>
-#include "LittleFS.h"
+#include <LittleFS.h>
 
-#include "WiFiManager.h"
-#include "webServer.h"
-#include "updater.h"
-#include "fetch.h"
-#include "configManager.h"
-#include "timeSync.h"
+#include <WiFiManager.h>
+#include <webServer.h>
+#include <updater.h>
+#include <fetch.h>
+#include <configManager.h>
+#include <timeSync.h>
+#include <dashboard.h>
+#include <TZ.h>
+
+struct task
+{    
+    unsigned long rate;
+    unsigned long previous;
+};
+
+task taskA = { .rate = 500, .previous = 0 };
 
 void setup()
 {
@@ -15,7 +25,23 @@ void setup()
     GUI.begin();
     configManager.begin();
     WiFiManager.begin(configManager.data.projectName);
-    timeSync.begin();
+
+    //Set the timezone to Amsterdam
+    timeSync.begin(TZ_Africa_Johannesburg);
+    timeSync.waitForSyncResult(10000);
+
+    if (timeSync.isSynced())
+    {
+        time_t now = time(nullptr);
+        Serial.print(PSTR("Current time in South Africa: "));
+        Serial.print(asctime(localtime(&now)));
+    }
+    else 
+    {
+        Serial.println("Timeout while receiving the time");
+    }
+
+    dash.begin(500);
 }
 
 void loop()
@@ -23,4 +49,26 @@ void loop()
     //software interrupts
     WiFiManager.loop();
     updater.loop();
+    configManager.loop();
+    dash.loop();
+
+    //your code here
+    //task A
+    if (taskA.previous == 0 || (millis() - taskA.previous > taskA.rate))
+    {
+        taskA.previous = millis();
+
+        String stringOne = "Apples";
+        stringOne.toCharArray(dash.data.projectName,32);
+        
+        dash.data.dummyInt++;
+        dash.data.inputInt++;
+
+        dash.data.dummyFloat = sin((float)millis()/1000);
+
+        if (dash.data.inputBool)
+            dash.data.dummyBool = true;
+        else
+            dash.data.dummyBool = false;
+    }
 }
